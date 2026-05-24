37523c72a99b197d308f08f26e11f03a1c91f7b6
yanxin-star <yanxin-2007@qq.com>
2026-05-24 15:43:40 +0800

    增加了如果识别不到块就抬高机械臂的分支

---
+++ src/task_game/include/nodes/lift_search.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "core/behavior_tree.hpp"
+
+#include <atomic>
+
+#include <rclcpp/rclcpp.hpp>
+#include <robot_interfaces/msg/armmode.hpp>
+
+class Robot;
+
+class CatchBoxAction : public BT::ActionNode {
+public:
+
+    CatchBoxAction();
+
+    void arm_cmd_callback(
+        const robot_interfaces::msg::Armmode::SharedPtr msg);
+
+protected:
+
+    BT::Status execute(BT& tree) override;
+
+private:
+
+    rclcpp::Publisher<
+        robot_interfaces::msg::Armmode>::SharedPtr arm_cmd_pub_;
+
+    rclcpp::Subscription<
+        robot_interfaces::msg::Armmode>::SharedPtr arm_state_sub_;
+
+    std::atomic<int> arm_search_state_{0};
+
+    float x{0.0f}, y{0.0f}, z{0.0f};
+
+    bool ros_initialized_ = false;
+};
