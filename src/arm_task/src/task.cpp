7b28eaf8496eb72cea5c5e6f443fe36b44642947
lyz1202dt <cloud1202@qq.com>
2026-06-18 10:18:24 +0800

    修正抓块时的z坐标错误

--- src/arm_task/src/task.cpp
+++ src/arm_task/src/task.cpp
@@ -312,7 +312,7 @@ void ArmTaskNode::execute_grasp_flow() {
     geometry_msgs::msg::PoseStamped object_pose;
     object_pose.pose.position.x=transfer.transform.translation.x;
     object_pose.pose.position.y=transfer.transform.translation.y;
-    object_pose.pose.position.z=transfer.transform.translation.z;
+    object_pose.pose.position.z=-0.23;
     tf2::Quaternion quat;
     quat.setRPY(0, M_PI / 2.0-0.25, 0);
     object_pose.pose.orientation.w = quat.getW();
