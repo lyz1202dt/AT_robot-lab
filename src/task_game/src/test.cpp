024a3a9a8a0c8db3766672c6e7a69f5f4b42de84
QPZ-918 <2941867312@qq.com>
2026-06-20 08:59:35 +0800

    位置坐标优化

--- src/task_game/src/test.cpp
+++ src/task_game/src/test.cpp
@@ -31,8 +31,8 @@ private:
         // 示例：两排各4个箱子, data[0~3] 第一排, data[4~7] 第二排
         // 同一个 ID 可出现多次（如两个箱子属于同一 ID 的放置列）
         grid_msg.data = {
-            0, 0, 0, 0,   // 第一排: box_id = 0,1,3,2
-            0, 0, 0, 0    // 第二排: box_id = 2,0,1,3
+            3, 2, 1, 0,   // 第一排: box_id = 0,1,3,2
+            0, 1, 2, 3    // 第二排: box_id = 2,0,1,3
         };
         grid_pub_->publish(grid_msg);
         //RCLCPP_INFO(this->get_logger(), "发布 box_id_grid");
